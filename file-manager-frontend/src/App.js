ac99bfa1794b84cc32fd866867ad264fce6a8bc7
Shashi <96482597+sandycloud@users.noreply.github.com>
2025-03-07 14:17:20 +0530

    Add files via upload

---
+++ file-manager-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileManager from './components/FileManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FileManager />
+    </div>
+  );
+}
+
+export default App;
